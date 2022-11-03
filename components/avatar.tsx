488123ce3edcf92c72b49c3d0829e5eab4f35496
Neeraj Dalal <admin@nrjdalal.com>
2022-11-03 12:55:30 +0530

    Thu Nov  3 12:55:30 IST 2022

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,22 +1,15 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Avatar({ author }) {
   const isAuthorHaveFullName = author?.node?.firstName && author?.node?.lastName
-  const name = isAuthorHaveFullName
-    ? `${author.node.firstName} ${author.node.lastName}`
-    : author.node.name || null
+  const name = isAuthorHaveFullName ? `${author.node.firstName} ${author.node.lastName}` : author.node.name || null
 
   return (
     <div className="flex items-center">
-      <div className="w-12 h-12 relative mr-4">
-        <Image
-          src={author.node.avatar.url}
-          className="rounded-full"
-          alt={name}
-          fill
-          sizes="100vw" />
-      </div>
-      <div className="text-xl font-bold">{name}</div>
+      {/* <div className="w-6 h-6 relative mr-4">
+        <Image src={author.node.avatar.url} className="rounded-full" alt={name} fill sizes="100vw" />
+      </div> */}
+      <div className="font-medium text-lg">{name}</div>
     </div>
-  );
+  )
 }
--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -7,13 +7,14 @@ export default function PostPreview({ title, coverImage, date, excerpt, author, 
   return (
     <div>
       <div className="mb-5">{coverImage && <CoverImage title={title} coverImage={coverImage} slug={slug} />}</div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3 className="text-2xl font-medium mb-3 leading-snug">
         <Link href={`/posts/${slug}`} className="hover:underline" dangerouslySetInnerHTML={{ __html: title }}></Link>
       </h3>
-      <div className="text-lg mb-4">
-        <Date dateString={date} />
-      </div>
       <div className="text-lg leading-relaxed mb-4 line-clamp-2" dangerouslySetInnerHTML={{ __html: excerpt }} />
+      <div className="mb-4 flex justify-between items-center">
+        <Date dateString={date} />
+        <Avatar author={author} />
+      </div>
       {/* <Avatar author={author} /> */}
     </div>
   )
