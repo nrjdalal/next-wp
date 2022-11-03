b6d39d0eb7cc5b0b9947a002aeccec5ea30e053c
Neeraj Dalal <admin@nrjdalal.com>
2022-11-03 12:40:26 +0530

    Thu Nov  3 12:40:26 IST 2022

--- components/alert.tsx
+++ components/alert.tsx
@@ -15,25 +15,13 @@ export default function Alert({ preview }) {
           {preview ? (
             <>
               This is a page preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
+              <a href="/api/exit-preview" className="underline hover:text-cyan duration-200 transition-colors">
                 Click here
               </a>{' '}
               to exit preview mode.
             </>
           ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
+            ''
           )}
         </div>
       </Container>
